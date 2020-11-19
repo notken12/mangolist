d6084af9d3ff273499795827bc5ec9be1ec9e842
Ken Zhou <kendotzhou@gmail.com>
2020-11-19 09:28:18 -0500

    basic js structure

--- index.js
+++ index.js
@@ -7,10 +7,11 @@
     }
 
     class TaskGroup {
-        constructor(name, tasks, done) {
+        constructor(name, tasks, done, el) {
             this.name = name || "";
             this.tasks = tasks || [];
             this.done = done || false;
+            this.el = el;
         }
         addTask(task) {
             this.tasks.push(task);
@@ -22,39 +23,79 @@
         TASKINPUT: $("#task-input"),
         CREATETASK: $("#create-task"),
         TASKAREA: $("#task-area"),
-        updateView: function () {
+        updateView: function (group) {
             var that = this;
+            var ul, title;
 
-            this.TASKAREA.empty();
-
-            taskGroups.forEach(function(group) {
+            if (!group.el) {
                 var div = document.createElement("div");
-                var ul = document.createElement("ul");
+                group.el = $(div);
+                group.el.addClass("task-group")
 
-                group.tasks.forEach(function(task) {
-                    var li = document.createElement("li");
-                    li.innerText = task.content;
+                title = $(document.createElement("h2"));
+                group.el.append(title);
 
-                    ul.appendChild(li);
-                });
-                div.appendChild(ul);
+                ul = $(document.createElement("ul"));
+                group.el.append(ul);
+                that.TASKAREA.append(group.el);
 
-                that.TASKAREA.append(div);
+            }
+            else {
+
+                ul = group.el.find("ul");
+                title = group.el.find("h2");
+
+                ul.empty();
+            }
+
+            title.text(group.name);
+
+            group.tasks.forEach(function(task) {
+                var li = document.createElement("li");
+                var p = document.createElement("p");
+
+                p.innerText = task.content;
+
+                var checkbox = document.createElement("input");
+                checkbox.type = "checkbox";
+                li.appendChild(checkbox);
+                li.appendChild(p);
+
+                ul.append(li);
             });
         }
     }
 
+    function isNullOrWhitespace(str) {
+        return (!str || str.length === 0 || /^\s*$/.test(str))
+    }
+
     const taskGroups = [];
+    taskGroups.updateAllViews = function () {
+        taskGroups.forEach(function (group) {
+            el.updateView(group);
+        });
+    }
+
     const mainGroup = new TaskGroup("Tasks");
+    const d = new TaskGroup("bruh tasks");
 
     taskGroups.push(mainGroup);
+    taskGroups.push(d);
+
+    d.addTask(new Task("pet mango"));
+
+    taskGroups.updateAllViews();
 
     function newTaskFromInput() {
+        if (isNullOrWhitespace(el.TASKINPUT.val()))
+            return;
+
         var task = new Task(el.TASKINPUT.val());
         mainGroup.addTask(task);
 
         el.TASKINPUT.val("");
-        el.updateView();
+        el.updateView(mainGroup);
     }
 
     el.CREATETASK.click(newTaskFromInput);
